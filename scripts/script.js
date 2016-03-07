96307fa29360c997c2119a3d9939fc9e01d2318f
Andrew Abarykov <expfront@gmail.com>
2016-03-07 09:41:34 +0300

    wave

--- scripts/script.js
+++ scripts/script.js
@@ -78,32 +78,9 @@ function updateAnalysers(time) {
 		}
 
 		// analyzer draw code here
-		{
-				var SPACING = 3;
-				var BAR_WIDTH = 1;
-				var numBars = Math.round(canvasWidth / SPACING);
-				var freqByteData = new Uint8Array(analyserNode.frequencyBinCount);
+		var canvas = document.getElementById( 'wavedisplay' );
 
-				analyserNode.getByteFrequencyData(freqByteData);
-
-				analyserContext.clearRect(0, 0, canvasWidth, canvasHeight);
-				analyserContext.fillStyle = '#F6D565';
-				analyserContext.lineCap = 'round';
-				var multiplier = analyserNode.frequencyBinCount / numBars;
-
-				// Draw rectangle for each frequency bin.
-				for (var i = 0; i < numBars; ++i) {
-						var magnitude = 0;
-						var offset = Math.floor( i * multiplier );
-						// gotta sum/average the block, or we miss narrow-bandwidth spikes
-						for (var j = 0; j< multiplier; j++)
-							magnitude += freqByteData[offset + j];
-						magnitude = magnitude / multiplier;
-						var magnitude2 = freqByteData[i * multiplier];
-						analyserContext.fillStyle = 'hsl( ' + Math.round((i * 360)/numBars) + ', 100%, 50%)';
-						analyserContext.fillRect(i * SPACING, canvasHeight, BAR_WIDTH, -magnitude);
-				}
-		}
+		drawBuffer( canvas.width, canvas.height, canvas.getContext('2d'), buffers[0] );
 
 		rafID = window.requestAnimationFrame( updateAnalysers );
 }
