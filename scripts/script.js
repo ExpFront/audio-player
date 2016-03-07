503a0c6fd15133f3450ff2233af9d8db5dcd053d
Andrew Abarykov <expfront@gmail.com>
2016-03-07 22:45:55 +0300

    date fix

--- scripts/script.js
+++ scripts/script.js
@@ -103,8 +103,8 @@ function updateAnalysers(time) {
 	}
 	var recorder_duration = document.querySelector('.recorder-duration');
 	var date = new Date();
-	console.log(date.getSeconds());
-	var duration = (date.getSeconds() - timeStart.getSeconds());
+	console.log(date.getMilliseconds());
+	var duration = (date.getMilliseconds() - timeStart.getMilliseconds()) * 0.01;
 	recorder_duration.innerHTML = duration;
 	// analyzer draw code here
 	{
