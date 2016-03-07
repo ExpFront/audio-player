25f39bb0ac688b60e982972fc79cf96aa95eb5bd
Andrew Abarykov <expfront@gmail.com>
2016-03-07 11:43:06 +0300

    fix styles

--- scripts/script.js
+++ scripts/script.js
@@ -51,7 +51,6 @@ function toggleRecording( e ) {
 				recorder.style.display = 'block';
 				audioRecorder.clear();
 				audioRecorder.record();
-				audioRecorder.getBuffers( gotBuffers );
 		}
 }
 
@@ -79,10 +78,33 @@ function updateAnalysers(time) {
 		}
 
 		// analyzer draw code here
-		{var canvas = document.getElementById( 'wavedisplay' );
+		{
+				var SPACING = 3;
+				var BAR_WIDTH = 1;
+				var numBars = Math.round(canvasWidth / SPACING);
+				var freqByteData = new Uint8Array(analyserNode.frequencyBinCount);
 
-		drawBuffer( canvas.width, canvas.height, canvas.getContext('2d'), buffers[0] );
-	}
+				analyserNode.getByteFrequencyData(freqByteData);
+
+				analyserContext.clearRect(0, 0, canvasWidth, canvasHeight);
+				analyserContext.fillStyle = '#F6D565';
+				analyserContext.lineCap = 'round';
+				var multiplier = analyserNode.frequencyBinCount / numBars;
+
+				// Draw rectangle for each frequency bin.
+				for (var i = 0; i < numBars; ++i) {
+						var magnitude = 0;
+						var offset = Math.floor( i * multiplier );
+						// gotta sum/average the block, or we miss narrow-bandwidth spikes
+						for (var j = 0; j< multiplier; j++)
+							magnitude += freqByteData[offset + j];
+						magnitude = magnitude / multiplier;
+						var magnitude2 = freqByteData[i * multiplier];
+						analyserContext.fillStyle = 'hsl( ' + Math.round((i * 360)/numBars) + ', 100%, 50%)';
+						analyserContext.fillRect(i * SPACING, canvasHeight, BAR_WIDTH, -magnitude);
+				}
+		}
+
 		rafID = window.requestAnimationFrame( updateAnalysers );
 }
 
