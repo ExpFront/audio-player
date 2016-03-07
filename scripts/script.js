cc1ba5dd234f194867b2ee9040b726b3fb4d56d5
Andrew Abarykov <expfront@gmail.com>
2016-03-07 15:57:47 +0300

    recorder bootstrap

--- scripts/script.js
+++ scripts/script.js
@@ -12,6 +12,7 @@ var recIndex = 0;
 
 // Elements
 var recorder_body = document.querySelector('.recorder-body');
+console.log(recorder_body);
 var recorder_footer = document.querySelector('.recorder-footer');
 var btn_record = document.querySelector('.btn-record');
 var button_type_play = document.getElementsByClassName('button_type_play')[0];
