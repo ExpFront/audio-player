9f86cb69ccb5f56ee46baa55315c117a51e1e71d
Andrew Abarykov <expfront@gmail.com>
2016-03-09 17:22:12 +0300

    fix

--- scripts/script.js
+++ scripts/script.js
@@ -106,30 +106,48 @@ function updateAnalysers(time) {
 	recorder_duration.innerHTML = getDuration(startTime);
 	// analyzer draw code here
 	{
-	var drawVisual = requestAnimationFrame(draw);
+		WIDTH = canvas.width;
+	HEIGHT = canvas.height;
 
-	analyser.getByteTimeDomainData(dataArray);
 
-	canvasCtx.fillStyle = 'rgb(200, 200, 200)';
-	canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
+	var visualSetting = visualSelect.value;
+	console.log(visualSetting);
 
-	canvasCtx.lineWidth = 2;
-	canvasCtx.strokeStyle = 'rgb(0, 0, 0)';
+	if(visualSetting == "sinewave") {
+		analyser.fftSize = 2048;
+		var bufferLength = analyser.fftSize;
+		console.log(bufferLength);
+		var dataArray = new Uint8Array(bufferLength);
 
-	canvasCtx.beginPath();
+		canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
 
-	var sliceWidth = WIDTH * 1.0 / bufferLength;
-	var x = 0;
+		function draw() {
 
-	for(var i = 0; i < bufferLength; i++) {
+			var drawVisual = requestAnimationFrame(draw);
 
-		var v = dataArray[i] / 128.0;
-		var y = v * HEIGHT/2;
+			analyser.getByteTimeDomainData(dataArray);
 
-		if(i === 0) {
-			canvasCtx.moveTo(x, y);
-		} else {
-			canvasCtx.lineTo(x, y);
+			canvasCtx.fillStyle = 'rgb(200, 200, 200)';
+			canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
+
+			canvasCtx.lineWidth = 2;
+			canvasCtx.strokeStyle = 'rgb(0, 0, 0)';
+
+			canvasCtx.beginPath();
+
+			var sliceWidth = WIDTH * 1.0 / bufferLength;
+			var x = 0;
+
+			for(var i = 0; i < bufferLength; i++) {
+
+				var v = dataArray[i] / 128.0;
+				var y = v * HEIGHT/2;
+
+				if(i === 0) {
+					canvasCtx.moveTo(x, y);
+				} else {
+					canvasCtx.lineTo(x, y);
+				}
 		}
 	}
 
