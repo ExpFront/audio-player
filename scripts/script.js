e24f209422a96ccafa2718836fa0c8b923bf363e
Andrew Abarykov <expfront@gmail.com>
2016-03-09 19:50:00 +0300

    try ff js

--- scripts/script.js
+++ scripts/script.js
@@ -195,7 +195,7 @@ function initAudio() {
 	}
 
 	console.log(navigator.getUserMedia);
-	navigator.mediaDevices.getUserMedia(
+	navigator.getUserMedia(
 		{'audio': {
 			'mandatory': {
 				'googEchoCancellation': 'false',
