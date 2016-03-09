322717435ef710871734d3bc3d3de780e37d7296
Andrew Abarykov <expfront@gmail.com>
2016-03-09 18:17:04 +0300

    date fix

--- scripts/script.js
+++ scripts/script.js
@@ -140,7 +140,7 @@ function updateAnalysers(time) {
 function getDuration(startTime) {
 	var date = new Date();
 	var seconds = date - startTime;
-	var milliseconds = seconds * 0.1;
+	var milliseconds = seconds / 1000 ;
 	console.log(milliseconds);
 	var duration = (milliseconds < 10 ? '0' + milliseconds : milliseconds);
 
