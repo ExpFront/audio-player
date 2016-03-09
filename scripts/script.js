0d46048f2c0bb01d02b6e65ca7c6015ae5363443
Andrew Abarykov <expfront@gmail.com>
2016-03-09 13:17:29 +0300

    wave fix

--- scripts/script.js
+++ scripts/script.js
@@ -108,35 +108,29 @@ function updateAnalysers(time) {
 	{
 		var SPACING = 3;
 		var BAR_WIDTH = 1;
-		var step = Math.ceil( data.length / width );
-		var amp = canvasHeight / 2;
 		var numBars = Math.round(canvasWidth / SPACING);
 		var freqByteData = new Uint8Array(analyserNode.frequencyBinCount);
 
 		analyserNode.getByteFrequencyData(freqByteData);
 
 		analyserContext.clearRect(0, 0, canvasWidth, canvasHeight);
-		context.fillStyle = '#373A3C';
-		context.lineWidth = 20;
+		analyserContext.fillStyle = '#373A3C';
+		analyserContext.lineCap = 'round';
+		var multiplier = analyserNode.frequencyBinCount / numBars;
 
 		// Draw rectangle for each frequency bin.
-		for (var i = 0; i < width; i++) {
-			var min = 1.0;
-			var max = -1.0;
-
-			for (j = 0; j < step; j++) {
-				var datum = data[(i * step) + j];
-
-				if (datum < min) {
-					min = datum;
-				}
-
-				if (datum > max) {
-					max = datum;
-				}
+		for (var i = 0; i < numBars; ++i) {
+			var magnitude = 0;
+			var offset = Math.floor( i * multiplier );
+			// gotta sum/average the block, or we miss narrow-bandwidth spikes
+			for (var j = 0; j < multiplier; j++) {
+				magnitude += freqByteData[offset + j];
 			}
 
-			analyserNode.fillRect(i, (1 + min) * amp, 1, Math.max(1, (max - min) * amp));
+			magnitude = magnitude / multiplier;
+			var magnitude2 = freqByteData[i * multiplier];
+			analyserContext.fillStyle = 'hsl( ' + Math.round((i * 360) / numBars) + ', 100%, 50%)';
+			analyserContext.fillRect(i * SPACING, canvasHeight, BAR_WIDTH, -magnitude);
 		}
 	}
 
