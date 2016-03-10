5dec87c334fbb899ac70899e0edf40b8c3933f81
Andrew Abarykov <expfront@gmail.com>
2016-03-10 11:23:02 +0300

    wave

--- js/audiodisplay.js
+++ js/audiodisplay.js
@@ -4,6 +4,8 @@ function drawBuffer( width, height, context, data ) {
 	context.fillStyle = '#373A3C';
 	context.lineWidth = 2;
 	context.clearRect(0, 0, width, height);
+	console.log(step);
+	console.log(amp);
 	for (var i = 0; i < width; i++) {
 		var min = 1.0;
 		var max = -1.0;
