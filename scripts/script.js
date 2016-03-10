782962a0f822383680b0cc911a4bc99714ef2250
Andrew Abarykov <expfront@gmail.com>
2016-03-10 09:46:37 +0300

    wave

--- scripts/script.js
+++ scripts/script.js
@@ -160,7 +160,7 @@ function updateAnalysers(time) {
 		analyserContext.fillStyle = '#373A3C';
 		analyserContext.lineWidth = 2;
 		analyserContext.clearRect(0, 0, canvasWidth, canvasHeight);
-		analyserContext.fillStyle = 'rgb(200, 200, 200)';
+		analyserContext.fillStyle = '#373A3C';
 
 		for (var i = 0; i < canvasWidth; i++) {
 			var min = 1.0;
