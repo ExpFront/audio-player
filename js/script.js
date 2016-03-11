51b0a6f418d03031c609f63398a6d27a6cb7f72c
Andrew Abarykov <expfront@gmail.com>
2016-03-11 18:29:03 +0300

    new wave

--- js/script.js
+++ js/script.js
@@ -104,7 +104,7 @@ function cancelAnalyserUpdates() {
 
 function updateAnalysers(time) {
 	if (!analyserContext) {
-			var canvas = document.getElementById('analyser');
+			var canvas = document.querySelector('.waveform');
 			canvasWidth = canvas.width;
 			canvasHeight = canvas.height;
 			analyserContext = canvas.getContext('2d');
